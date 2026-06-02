a92195e772baa6717b59376b9eea8adee859c75a
Lara-Mariah1 <Lara.benato@escola.pr.gov.br>
2026-06-02 12:21:59 -0300

    lara

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Agrinho',
+    'Como o fertilizante natural ajuda o solo de verdade?',
+    'Melhora a areação.'
+)
+
+criaCartao(
+    'Agrinhho'
+    'Como produzir mais alimentos sem desmatar novas áreas?' ,
+    'Aplicar a intensificação saudável.'
+)
+
+criaCartao(
+    'Agrinho',
+    'É possível lucrar alto protegendo a natureza? ',
+    'Irrigação e sistemas autotizado.'
+)
+
+criaCartao(
+    'Agrinho',
+    ' É possível lucrar alto protegendo a natureza?' ,
+    ' Sim. '
+)
+
+criaCartao(
+    'Agrinho',
+   'Como a rotação de culturas melhora a terra?'
+    'repor matéria orgânica, fixar nutrientes essenciais, melhorar a estrutura física do solo e quebrar o ciclo de pragas e doenças.' ,
+)
+criaCartao(
+    'Agrinho',
+    'De que forma o agro ajuda a reduzir o carbono no ar?' ,
+    'O agronegócio ajuda a retirar o carbono do ar principalmente por meio da agricultura de baixo carbono e da recuperação de pastagens.' ,
+)
+
+criaCartao(
+    'Agrinho',
+    'Como treinar os trabalhadores para a tecnologia verde?',
+    'O Akita Inu.'
+)
+
+criaCartao(
+    'Agrinho',
+    'Qual cachorro é famoso por suas dobras na pele e rosto enrugado?',
+    '.conscientização ambiental, requalificação técnica (green skills) e aplicação prática na rotina.'
+)
+
+criaCartao(
+    'Agrinho',
+    'Qual é o papel das árvores no meio da plantação?' ,
+    'focar em receitas caseiras naturais, plantio preventivo e controle biológico.'
+)
+
+criaCartao(
+    'Agrinho',
+    'Como combater as pragas sem usar veneno químico?',
+    'O Fila Brasileiro.'
+)
+
+criaCartao(
+    'Agrinho',
+    'Qual cachorro é usado como guia para pessoas cegas em muitos países?',
+    'O Labrador Retriever..'
+)
+
+criaCartao(
+    'Agrinho',
+    'Qual é a raça com olfato mais poderoso, usada em resgates e farejamentos?',
+    'O Bloodhound.'
+)
